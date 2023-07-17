d0f7a63dd2e65dcf188cf6821accf0048b88548d
Hiroshi Ito <hiroshi.ito.mk2@gmail.com>
2023-07-17 16:41:39 +0900

    #フォームとのバインド

--- src/main/java/com/example/SpringBootSample/controller/SignupController.java
+++ src/main/java/com/example/SpringBootSample/controller/SignupController.java
@@ -1,10 +1,13 @@
 package com.example.SpringBootSample.controller;
 
 import com.example.SpringBootSample.application.service.UserApplicationService;
+import com.example.SpringBootSample.form.SignupForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,13 +16,14 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 
     @Autowired
     private UserApplicationService userApplicationService;
 
     @GetMapping("/signup")
-    public String getSignup(Model model, Locale locale) {
+    public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
 
         Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap", genderMap);
@@ -27,8 +31,8 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String postSignup() {
-
+    public String postSignup(@ModelAttribute SignupForm form) {
+        log.info(form.toString());
         return "redirect:/login";
     }
 
