18f8f744d1ad9c3c08fffaf84f8b8ff4776e7584
Hiroshi Ito <hiroshi.ito.mk2@gmail.com>
2023-09-06 11:44:28 +0900

    ignore修正

--- docker/db/initdb/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE spring_sample;
-
-\c spring_sample
-
-CREATE SCHEMA dev;
-
-CREATE USER developer WITH PASSWORD 'dke@keig24' CREATEDB;
-
-GRANT ALL PRIVILEGES ON SCHEMA dev TO developer;
